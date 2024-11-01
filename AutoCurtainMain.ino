493d4c4ec720c863f63ce7665a032adc3055b07a
tjm7098 <tjm7098@psu.edu>
2024-11-01 10:44:49 -0400

    Add files via upload

---
+++ AutoCurtainMain.ino
@@ -0,0 +1,19 @@
+// AutoCurtainMain.ino
+#include "state_transition.h"
+#include "motor_drive.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+  void drive_setup();
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (){
+    void drive(DIRECTION STOP); // {STOP, OPEN, CLOSE}
+  }
+  void drive(STOP);
+}
