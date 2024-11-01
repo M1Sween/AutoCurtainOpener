67a866931ca0e31a13e62ca6356d7573c5b441bb
tjm7098 <tjm7098@psu.edu>
2024-11-01 17:07:39 -0400

    Update AutoCurtainMain.ino

--- AutoCurtainMain.ino
+++ AutoCurtainMain.ino
@@ -7,13 +7,13 @@ void setup() {
   // put your setup code here, to run once:
   void drive_setup();
 
-
+  void myfunc(int 2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   while (){
-    void drive(DIRECTION OPEN); // {STOP, OPEN, CLOSE}
+    void drive(OPEN); // {STOP, OPEN, CLOSE}
   }
   void drive(STOP);
 }
